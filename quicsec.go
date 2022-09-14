03ba326e644ad0bbfae3d0912cf6dbdb15f914b0
Thales Paiva <trauervoll@gmail.com>
2022-09-14 19:42:03 -0300

    Creating conn package

--- quicsec.go
+++ quicsec.go
@@ -1,24 +1,11 @@
 package quicsec
 
 import (
-	"bufio"
-	"fmt"
-	"io"
-	"log"
 	"net/http"
-	"os"
 	"sync"
-	"net"
-	"crypto/tls"
+	"fmt"
 
-	"github.com/lucas-clemente/quic-go"
-	"github.com/lucas-clemente/quic-go/http3"
-	"github.com/lucas-clemente/quic-go/logging"
-	"github.com/lucas-clemente/quic-go/qlog"
-
-	"github.com/quicsec/quicsec/utils"
-	"github.com/quicsec/quicsec/auth"
-	"github.com/quicsec/quicsec/identity"
+	"github.com/quicsec/quicsec/conn"
 )
 
 type binds []string
@@ -38,7 +25,7 @@ func ListenAndServe(bs []string, handler http.Handler) error {
 		go func() {
 			fmt.Printf("Starting QUIC listener on %s...\n", bs)
 
-			err = listenAndServe(bCap, handler)
+			err = conn.ListenAndServe(bCap, handler)
 
 			if err != nil {
 				fmt.Println(err)
@@ -49,96 +36,4 @@ func ListenAndServe(bs []string, handler http.Handler) error {
 	wg.Wait()
 
 	return err
-}
-
-func listenAndServe(addr string, handler http.Handler) error {
-	// Load certs
-	var err error
-	enableQlog := true// configurable
-	certFile, keyFile := identity.GetIndentityPaths()
-
-	certs := make([]tls.Certificate, 1)
-	certs[0], err = tls.LoadX509KeyPair(certFile, keyFile)
-	if err != nil {
-		return err
-	}
-
-	tlsConfig := &tls.Config{
-		Certificates: certs,
-		VerifyPeerCertificate: auth.QuicsecVerifyPeerCertificate,
-	}
-
-	// Open the listeners
-	udpAddr, err := net.ResolveUDPAddr("udp", addr)
-	if err != nil {
-		return err
-	}
-	udpConn, err := net.ListenUDP("udp", udpAddr)
-	if err != nil {
-		return err
-	}
-	defer udpConn.Close()
-
-	tcpAddr, err := net.ResolveTCPAddr("tcp", addr)
-	if err != nil {
-		return err
-	}
-	tcpConn, err := net.ListenTCP("tcp", tcpAddr)
-	if err != nil {
-		return err
-	}
-	defer tcpConn.Close()
-
-	tlsConn := tls.NewListener(tcpConn, tlsConfig)
-	defer tlsConn.Close()
-
-	if handler == nil {
-		handler = http.DefaultServeMux
-	}
-
-	quicConf := &quic.Config{}
-
-	if enableQlog {
-		quicConf.Tracer = qlog.NewTracer(func(_ logging.Perspective, connID []byte) io.WriteCloser {
-			filename := fmt.Sprintf("qlog/server_%x.qlog", connID)
-			f, err := os.Create(filename)
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Printf("Creating qlog file %s.\n", filename)
-			return utils.NewBufferedWriteCloser(bufio.NewWriter(f), f)
-		})
-	}
-
-	// Start the servers
-	quicServer := &http3.Server{
-		TLSConfig: tlsConfig,
-		Handler: handler,
-		QuicConfig: quicConf,
-	}
-
-	httpServer := &http.Server{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			quicServer.SetQuicHeaders(w.Header())
-			handler.ServeHTTP(w, r)
-		}),
-	}
-
-	hErr := make(chan error)
-	qErr := make(chan error)
-	go func() {
-		hErr <- httpServer.Serve(tlsConn)
-	}()
-	go func() {
-		qErr <- quicServer.Serve(udpConn)
-	}()
-
-	select {
-	case err := <-hErr:
-		quicServer.Close()
-		return err
-	case err := <-qErr:
-		// Cannot close the HTTP server or wait for requests to complete properly :/
-		return err
-	}
 }
