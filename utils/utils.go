a7c5dafab426493f29eeaadd153cfb618e08e0a2
Raphael Campos <raphael@pod8.io>
2023-05-04 10:08:00 -0300

    Fix logfile/acessfile path
    - Check if a log output file or access log exists or if is it possible
    to create both files. If not, use stdout instead for each case;
    - Also, show the path for output/access in the logs.

---
+++ utils/utils.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"io/ioutil"
+	"os"
+)
+
+// GetEnv is a convenience wrapper for os.Getenv() with additional default value return
+// when empty or unset
+func GetEnv(envVar string, defaultValue string) string {
+	val := os.Getenv(envVar)
+	if val == "" {
+		return defaultValue
+	}
+	return val
+}
+
+func IsValidPath(fp string) bool {
+	// Check if file already exists
+	if _, err := os.Stat(fp); err == nil {
+		return true
+	}
+
+	// Attempt to create it
+	var d []byte
+	if err := ioutil.WriteFile(fp, d, 0644); err == nil {
+		os.Remove(fp) // And delete it
+		return true
+	}
+
+	return false
+}
