afae5568e9433898af73d621343b68df636973dc
Thales Paiva <trauervoll@gmail.com>
2023-11-03 16:45:08 +0000

    allow waf and authz filters to be configured by en vars.

--- http/filters/coraza_filter.go
+++ http/filters/coraza_filter.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+
+	. "github.com/quicsec/quicsec/http"
 
 	"github.com/corazawaf/coraza/v3"
 	"github.com/corazawaf/coraza/v3/types"
@@ -25,9 +28,50 @@ func NewCorazaFilter(config string) *CorazaFilter {
 
 	return  &CorazaFilter{Waf: waf}
 }
+func (c *CorazaFilter) loadWafConfig(id RequestIdentity) error {
+	var config string
+
+	switch id.Class {
+		case NO_IDENTITY:
+			config = os.Getenv("QUICSEC_WAF_NO_ID_RULES")
+		case UNK_IDENTITY:
+			config = os.Getenv("QUICSEC_WAF_UKN_ID_RULES")
+		case KNW_IDENTITY:
+			config = os.Getenv("QUICSEC_WAF_KNW_ID_RULES")
+		default:
+			config = os.Getenv("QUICSEC_WAF_DEFAULT_RULES")
+	}
+
+	if config == "" {
+		fmt.Println("no rules found for identiy class:", id.Class.String(), "loading default WAF rules")
+		config = os.Getenv("QUICSEC_WAF_DEFAULT_RULES")
+
+		if config == "" {
+			return fmt.Errorf("failed to load default WAF rules. The env variable QUICSEC_WAF_DEFAULT_RULES must be configured")
+		}
+	}
+
+	waf, err := coraza.NewWAF(
+		coraza.NewWAFConfig().
+			WithErrorCallback(logError).
+			WithDirectivesFromFile(config),
+	)
+	if err != nil {
+		return err
+	}
+	
+	c.Waf = waf
+
+	return nil
+}
 
 func (c *CorazaFilter) Execute(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) error {
-	
+	//we can chose the waf configuration given an identity
+	err := c.loadWafConfig(GetRequestIdentity(r))
+	if err != nil {
+		return err
+	}
+
 	tx := c.Waf.NewTransaction()
 
 	defer func() {
